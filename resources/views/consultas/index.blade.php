c55f3d613ca9e70020c37da0537cfbb9d163fb2f
Adrian Cardenas <SonHak@github.com>
2018-01-19 19:28:50 +0100

    projecte vota

---
+++ resources/views/consultas/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+    MOSTRAR TODAS LAS CONSULTAS
+
+@stop
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('login', function () {
+    return view('auth.login');
+});
+Route::get('logout', function () {
+    return view('pagina logout');
+});
+Route::get('consultas', function () {
+    return view('consultas.index');
+});
+Route::get('consultas/crear', function () {
+    return view('consultas.crear');
+});
+Route::get('consultas/votar/{id}', function ($id) {
+    return view('consultas.votar', array('id'=> $id));
+});
+Route::get('consultas/editar/{id}', function ($id) {
+    return view('consultas.editar', array('id'=> $id));
+});
